e834344079e032a13919ab1088952c64b2db2bc1
Aleksandr Kaigorodov <alexkaygorodov@gmail.com>
2022-05-31 01:09:24 +0300

    Добавил редактирование профиля

---
+++ pages/index.js
@@ -0,0 +1,48 @@
+const button = document.querySelector('.profile-text-info__editButton');
+const form = document.querySelector('.form');
+const formCloseButton = document.querySelector('.form-edit-profile__close-button');
+
+// console.log(button);
+// function closeFormOnQ(e) {
+//     if (e.code === 'KeyQ') {
+//         closeForm();
+//     }
+// }
+function openForm() {
+    form.classList.remove('form_hidden');
+    // document.addEventListener('keypress', closeFormOnQ);
+}
+function closeForm() {
+    form.classList.add('form_hidden');
+    // document.removeEventListener('keypress', closeFormOnQ);
+}
+
+button.addEventListener('click', function () {
+    openForm();
+});
+
+formCloseButton.addEventListener('click', function () {
+    closeForm();
+});
+
+form.addEventListener('click', function (e) {
+        if (e.target === e.currentTarget) {
+            closeForm();
+        }
+    });
+
+// --------------------------------------------
+let fullName = document.querySelector('.form-edit-profile__full-name');
+let description = document.querySelector('.form-edit-profile__description');
+
+const submitButton = document.querySelector('.form-edit-profile__submit-button');
+
+function saveButtonAction() {
+    document.querySelector('.profile-text-info__full-name').textContent = fullName.value;
+    document.querySelector('.profile-text-info__description').textContent = description.value;
+    closeForm();
+}
+
+submitButton.addEventListener('click', saveButtonAction);
+// --------------------------------------------
+
