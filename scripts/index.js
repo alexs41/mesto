b7ddb14688767a33994bb28209e98d68cfc8b62f
Aleksandr Kaigorodov <alexkaygorodov@gmail.com>
2022-06-06 01:53:49 +0300

    Правки после 1-го ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,14 @@
-const button = document.querySelector('.profile-info-container__editButton');
+const button = document.querySelector('.profile-info-container__edit-button');
 const popup = document.querySelector('.popup');
 const popupCloseButton = document.querySelector('.popup__close-button');
 
 function openPopup() {
     popup.classList.add('popup_opened');
-    // document.addEventListener('keypress', closePopupOnQ);
     fullName.value = document.querySelector('.profile-text-info__full-name').textContent;
     description.value = document.querySelector('.profile-text-info__description').textContent;
 }
 function closePopup() {
     popup.classList.remove('popup_opened');
-    // document.removeEventListener('keypress', closePopupOnQ);
 }
 
 button.addEventListener('click', function () {
@@ -27,12 +25,9 @@ popup.addEventListener('click', function (e) {
         }
     });
 
-// --------------------------------------------
-let fullName = document.querySelector('.form-edit-profile__full-name');
-let description = document.querySelector('.form-edit-profile__description');
+let fullName = document.querySelector('.form-edit-profile__input_full-name');
+let description = document.querySelector('.form-edit-profile__input_description');
 const submitButton = document.querySelector('.form-edit-profile__submit-button');
-
-
 
 function saveButtonAction() {
     document.querySelector('.profile-text-info__full-name').textContent = fullName.value;
@@ -41,12 +36,11 @@ function saveButtonAction() {
 }
 
 submitButton.addEventListener('click', saveButtonAction);
-// --------------------------------------------
 
-const likeButtons = Array.from(document.querySelectorAll('.element__likeButton'));
+const likeButtons = Array.from(document.querySelectorAll('.element__like-button'));
 
 likeButtons.forEach((button, index) => {
     button.addEventListener("click", () => {
-      button.classList.toggle('element__likeButton_active');
+      button.classList.toggle('element__like-button_active');
     });
   });
