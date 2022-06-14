da09d25c2ffe9615972b370a56a93fc4f4448b4b
Aleksandr Kaigorodov <alexkaygorodov@gmail.com>
2022-06-15 00:34:53 +0300

    добавил кнопку trash и popup по кнопке git add-element

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,66 @@
 const button = document.querySelector('.profile-info-container__edit-button');
 const popup = document.querySelector('.popup');
+const popupOpened = document.querySelector('.popup_opened');
+const addElementPopup = document.querySelector('.add-element-popup');
 const popupCloseButton = document.querySelector('.popup__close-button');
+const addElementButton = document.querySelector('.profile__add-button');
+
+const popups = document.querySelectorAll('.popup');
+const popupCloseButtons = document.querySelectorAll('.popup__close-button');
 
 function openPopup() {
     popup.classList.add('popup_opened');
     fullName.value = document.querySelector('.profile-text-info__full-name').textContent;
     description.value = document.querySelector('.profile-text-info__description').textContent;
 }
+
 function closePopup() {
-    popup.classList.remove('popup_opened');
+    let popupOpened = document.querySelector('.popup_opened');
+    popupOpened.classList.remove('popup_opened');
 }
+
+//------------------------ addElementButton start
+function openAddElementPopup() {
+    addElementPopup.classList.add('popup_opened');
+}
+addElementButton.addEventListener('click', function () {
+    openAddElementPopup();
+});
+//------------------------ addElementButton end
 
 button.addEventListener('click', function () {
     openPopup();
 });
 
-popupCloseButton.addEventListener('click', function () {
-    closePopup();
+// popupCloseButton.addEventListener('click', function () {
+//     closePopup();
+// });
+
+popupCloseButtons.forEach(function(button) {
+    button.addEventListener('click', function () {
+        closePopup();
+    });
 });
 
-popup.addEventListener('click', function (e) {
-        if (e.target === e.currentTarget) {
-            closePopup();
-        }
+// старая функция закрытия POPUP по клику на черный экран
+// popup.addEventListener('click', function (e) {
+//         if (e.target === e.currentTarget) {
+//             closePopup();
+//         }
+//     });
+popups.forEach(function(popup) {
+    popup.addEventListener('click', function (e) {
+             if (e.target === e.currentTarget) {
+        closePopup();
+        };
     });
+});
 
-let fullName = document.querySelector('.form-edit-profile__input_full-name');
-let description = document.querySelector('.form-edit-profile__input_description');
-const submitButton = document.querySelector('.form-edit-profile__submit-button');
+
+
+let fullName = document.querySelector('.form__input_full-name');
+let description = document.querySelector('.form__input_description');
+const submitButton = document.querySelector('.form__submit-button');
 
 function saveButtonAction() {
     document.querySelector('.profile-text-info__full-name').textContent = fullName.value;
